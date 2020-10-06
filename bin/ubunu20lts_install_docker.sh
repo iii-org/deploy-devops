cc696ee3caaf29fb32c82712d985c4c05baebaa3
YuWei Chou <demoyuw@gmail.com>
2020-10-06 10:36:44 +0800

    add ubuntu20lts_install_docker.sh

---
+++ bin/ubunu20lts_install_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WORK_DIR=/home/$USER
+LOGFILE=$WORK_DIR/log/docker_install.log
+mkdir $WORK_DIR/log
+touch $LOGFILE
+
+sudo apt-get update -y &>> $LOGFILE
+sudo apt-get install     apt-transport-https     ca-certificates     curl  gnupg-agent   software-properties-common -y &>> $LOGFILE
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" &>> $LOGFILE
+apt-get update -y &>> $LOGFILE 
+apt-get install docker-ce docker-ce-cli containerd.io -y &>> $LOGFILE
