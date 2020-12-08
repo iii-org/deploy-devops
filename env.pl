79e71b7fbbae7be90ee3c3f4593abf4bb854de7c
jonathan <jonathan@iii.org.tw>
2020-12-08 18:29:01 +0800

    新增 gitlab 建立的 perl script

---
+++ env.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+# 16:16 2020/12/08 Jonathan Tsai
+# Install iiidevops environment configuration 
+
+# GitLab External URL gitlab/create_gitlab.pl
+$gitlab_url = "140.92.4.3"; # if empty then auto setting gitlab server ip 
---
+++ gitlab/create_gitlab.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use Socket;
+use Sys::Hostname;
+
+my $p_config = '../env.pl';
+if (-e $p_config) {
+	require($p_config);
+}
+
+if (!defined($gitlab_url)) {
+	my $host = hostname();
+	$gitlab_url = inet_ntoa(scalar gethostbyname($host || 'localhost'));
+}
+
+print("Install GitLab URL: http://$gitlab_url\n");
+$cmd="sudo docker run --env GITLAB_OMNIBUS_CONFIG=\"external_url 'http://$gitlab_url';\"  --detach --publish 443:443 --publish 80:80 --publish 10022:22 --name gitlab --restart always --volume $HOME/gitlab/config:/etc/gitlab --volume $HOME/gitlab/logs:/var/log/gitlab --volume $HOME/gitlab/data:/var/opt/gitlab gitlab/gitlab-ce:12.10.6-ce.0";
+print("-----\n$cmd\n\n");
+
+$cmd_msg = `$cmd`;
+print("-----\n$cmd_msg\n\n");
