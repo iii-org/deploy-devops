5d174216e84e4db7ed32004f7265a9f04cd42f71
jonathan <jonathan@iii.org.tw>
2020-12-09 19:39:26 +0800

    Add the docker installation script directly

--- bin/iiidevops_install.pl
+++ bin/iiidevops_install.pl
@@ -6,7 +6,7 @@ print("Getting iiidevops Deploy Package..\n");
 $cmd_msg = `$cmd`;
 #print("-----\n$cmd_msg\n-----\n");
 
-$cmd = "sudo apt install unzip";
+$cmd = "sudo apt-get install unzip nfs-common -y";
 $cmd .= "; cd ~; unzip master.zip";
 $cmd .= "; cd deploy-devops-master/";
 $cmd .= "; chmod a+x bin/*.sh";
@@ -16,10 +16,22 @@ print("Unziping iiidevops Deploy Package..\n");
 $cmd_msg = `$cmd`;
 print("-----\n$cmd_msg\n-----\n");
 
-$cmd = "cd ~/deploy-devops-master/; sudo bin/ubuntu20lts_install_docker.sh";
+#$cmd = "cd ~/deploy-devops-master/; sudo bin/ubuntu20lts_install_docker.sh";
+$cmd = "sudo apt-get update -y ";
+$cmd .= "; sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y ";
+$cmd .= "; curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ";
+$cmd .= "; sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable\" "; 
+$cmd .= "; sudo apt-get update -y ";
+$cmd .= "; sudo apt-get install docker-ce docker-ce-cli containerd.io -y ";
+$cmd .= "; usermod -aG docker \$SUDO_USER ";
 $cmd .= "; docker -v";
 print("Install docker..\n");
 $cmd_msg = `$cmd`;
 print("-----\n$cmd_msg\n-----\n");
 
-print("Please Edit ~/deploy-devops-master/env.pl \n");
+$cmd = "sudo snap install kubectl --classic";
+print("Install kubectl..\n");
+$cmd_msg = `$cmd`;
+print("-----\n$cmd_msg\n-----\n");
+
+print("Please Edit ~/deploy-devops-master/env.pl \n\n");
