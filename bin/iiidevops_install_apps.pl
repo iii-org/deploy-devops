ccb935158f39e04f3b2bd288e63a2314acf290a8
jonathan <jonathan@iii.org.tw>
2020-12-10 19:07:15 +0800

    Add deploy sonarqube

--- bin/iiidevops_install_apps.pl
+++ bin/iiidevops_install_apps.pl
@@ -10,7 +10,7 @@ if (!-e $p_config) {
 require($p_config);
 
 # Deploy Redmine on kubernetes cluster
-# Modify redmine/redmine-postgresql/redmine-postgresql.yml.tmpl <- {{postgres_password}} {{nfs_ip}}
+# Modify redmine/redmine-postgresql/redmine-postgresql.yml.tmpl <- {{redmine_db_passwd}} {{nfs_ip}}
 $yaml_path = "$Bin/../redmine/redmine-postgresql/";
 $yaml_file = $yaml_path.'redmine-postgresql.yml';
 $tmpl_file = $yaml_file.'.tmpl';
@@ -19,7 +19,7 @@ if (!-e $tmpl_file) {
 	exit;
 }
 $template = `cat $tmpl_file`;
-$template =~ s/{{postgres_password}}/$postgres_password/g;
+$template =~ s/{{redmine_db_passwd}}/$redmine_db_passwd/g;
 $template =~ s/{{nfs_ip}}/$nfs_ip/g;
 #print("-----\n$template\n-----\n\n");
 open(FH, '>', $yaml_file) or die $!;
@@ -30,7 +30,7 @@ print("Deploy redmine-postgresql..\n");
 $cmd_msg = `$cmd`;
 print("-----\n$cmd_msg\n-----\n\n");
 
-# Modify redmine/redmine/redmine-deployment.yml.tmpl <- {{postgres_password}}
+# Modify redmine/redmine/redmine-deployment.yml.tmpl <- {{redmine_db_passwd}}
 $yaml_path = "$Bin/../redmine/redmine/";
 $yaml_file = $yaml_path.'redmine-deployment.yml';
 $tmpl_file = $yaml_file.'.tmpl';
@@ -39,7 +39,7 @@ if (!-e $tmpl_file) {
 	exit;
 }
 $template = `cat $tmpl_file`;
-$template =~ s/{{postgres_password}}/$postgres_password/g;
+$template =~ s/{{redmine_db_passwd}}/$redmine_db_passwd/g;
 #print("-----\n$template\n-----\n\n");
 open(FH, '>', $yaml_file) or die $!;
 print FH $template;
@@ -51,6 +51,23 @@ print("-----\n$cmd_msg\n-----\n\n");
 
 # Deploy SonarQube Server on kubernetes cluster
 # Modify sonarqube/sonar-server-deployment.yaml.tmpl <- {{nfs_ip}}
+$yaml_path = "$Bin/../sonarqube/";
+$yaml_file = $yaml_path.'sonar-server-deployment.yaml';
+$tmpl_file = $yaml_file.'.tmpl';
+if (!-e $tmpl_file) {
+	print("The template file [$tmpl_file] does not exist!\n");
+	exit;
+}
+$template = `cat $tmpl_file`;
+$template =~ s/{{nfs_ip}}/$nfs_ip/g;
+#print("-----\n$template\n-----\n\n");
+open(FH, '>', $yaml_file) or die $!;
+print FH $template;
+close(FH);
+$cmd = "kubectl apply -f $yaml_path";
+print("Deploy sonarqube..\n");
+$cmd_msg = `$cmd`;
+print("-----\n$cmd_msg\n-----\n\n");
 
 # Deploy DevOps DB (Postgresql) on kubernetes cluster
 # Modify devops-db/devopsdb-deployment.yaml.tmpl <- {{postgres_password}}
