4727b96118409c8498678bca04798fa0afbcbbbb
jonathan <tryweb@ichiayi.com>
2021-01-19 15:36:37 +0800

    Change rancher listen port from 6443 to 3443, 6080 to 3080

--- rancher/install_rancher.pl
+++ rancher/install_rancher.pl
@@ -14,9 +14,9 @@ $logfile = "$Bin/$prgname.log";
 log_print("\n----------------------------------------\n");
 log_print(`TZ='Asia/Taipei' date`);
 
-log_print("Install Rancher URL: https://$rancher_ip:6443\n");
+log_print("Install Rancher URL: https://$rancher_ip:3443\n");
 # Check Rancher is working
-$cmd_msg = `nc -z -v $rancher_ip 6443 2>&1`;
+$cmd_msg = `nc -z -v $rancher_ip 3443 2>&1`;
 $isWorking = index($cmd_msg, 'succeeded!')<0?0:1;
 if ($isWorking) {
 	log_print("Rancher is running, I skip the installation!\n\n");
@@ -28,14 +28,14 @@ log_print("-----\n$cmd\n");
 $cmd_msg = `$cmd`;
 log_print("-----\n$cmd_msg\n\n");
 
-$cmd = "sudo docker run -d --restart=unless-stopped -p 6080:80 -p 6443:443 -v $data_dir/rancher:/var/lib/rancher rancher/rancher:v2.4.5 &";
+$cmd = "sudo docker run -d --restart=unless-stopped -p 3080:80 -p 3443:443 -v $data_dir/rancher:/var/lib/rancher rancher/rancher:v2.4.5 &";
 log_print("-----\n$cmd\n");
 $cmd_msg = `$cmd`;
 log_print("-----\n$cmd_msg\n\n");
 
 # Check Rancher service is working
-$cmd = "nc -z -v $rancher_ip 6443";
-# Connection to 10.20.0.71 6443 port [tcp/*] succeeded!
+$cmd = "nc -z -v $rancher_ip 3443";
+# Connection to 10.20.0.71 3443 port [tcp/*] succeeded!
 $chk_key = 'succeeded!';
 $isChk=1;
 $count=0;
