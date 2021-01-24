fbf8fae48cec521fc0b2ce9c61b689d7cf87baf9
jonathan <tryweb@ichiayi.com>
2021-01-24 16:25:36 +0800

    Deploy Harbor on K8s

---
+++ harbor/install_harbor.pl
@@ -0,0 +1,192 @@
+#!/usr/bin/perl
+# Install Harbor service script
+#
+use FindBin qw($Bin);
+use MIME::Base64;
+my $p_config = "$Bin/../env.pl";
+if (!-e $p_config) {
+	print("The configuration file [$p_config] does not exist!\n\n");
+	exit;
+}
+require($p_config);
+
+if ($harbor_ip eq '') {
+	print("The harbor_ip in [$p_config] is ''!\n\n");
+	exit;
+}
+
+$prgname = substr($0, rindex($0,"/")+1);
+$logfile = "$Bin/$prgname.log";
+log_print("\n----------------------------------------\n");
+log_print(`TZ='Asia/Taipei' date`);
+
+if (lc($ARGV[0]) eq 'create_dockerhub_proxy') {
+	create_dockerhub_proxy();
+	exit;
+}
+
+$harbor_domain_name = ($harbor_domain_name eq '')?"harbor.iiidevops.$gitlab_ip.xip.io":$harbor_domain_name;
+log_print("Install Harbor URL: https://$harbor_domain_name\n");
+# Check Harbor is working
+$cmd_msg = `curl -k --location --request POST 'https://$harbor_domain_name/api/v2.0/registries' 2>&1`;
+#{"errors":[{"code":"UNAUTHORIZED","message":"UnAuthorized"}]}
+$isWorking = index($cmd_msg, 'UNAUTHORIZED')<0?0:1;
+if ($isWorking) {
+	log_print("Harbor is running, I skip the installation!\n\n");
+	exit;
+}
+
+# Generate install yaml and exec install.sh
+install_harbor();
+
+# Check Harbor service is working
+$cmd = "curl -s -k --location --request POST 'https://$harbor_domain_name/api/v2.0/registries'";
+#{"errors":[{"code":"UNAUTHORIZED","message":"UnAuthorized"}]}
+$chk_key = 'UNAUTHORIZED';
+$isChk=1;
+$count=0;
+$wait_sec=600;
+while($isChk && $count<$wait_sec) {
+	log_print('.');
+	$cmd_msg = `$cmd 2>&1`;
+	$isChk = (index($cmd_msg, $chk_key)<0)?1:0;
+	$count ++;
+	sleep($isChk);
+}
+log_print("\n");
+if ($isChk) {
+	log_print("Failed to deploy Harbor!\n");
+	log_print("-----\n$cmd_msg-----\n");
+	exit;
+}
+log_print("Successfully deployed Harbor!\n");
+
+# create dockerhub proxy project
+create_dockerhub_proxy();
+
+exit;
+
+
+sub install_harbor {
+# Deploy Harbor on kubernetes cluster
+
+	# Modify harbor/harbor-lite-install.yaml.tmpl
+	$yaml_path = "$Bin/../harbor/";
+	$yaml_file = $yaml_path.'harbor-lite-install.yaml';
+	$tmpl_file = $yaml_file.'.tmpl';
+	if (!-e $tmpl_file) {
+		log_print("The template file [$tmpl_file] does not exist!\n");
+		exit;
+	}
+	$template = `cat $tmpl_file`;
+	$template =~ s/{{harbor_admin_password}}/$harbor_admin_password/g;
+	$template =~ s/{{harbor_domain_name}}/$harbor_domain_name/g;
+	#log_print("-----\n$template\n-----\n\n");
+	open(FH, '>', $yaml_file) or die $!;
+	print FH $template;
+	close(FH);
+	# Modify harbor/nfs-client-provisioner-pv.yaml.tmpl
+	$yaml_path = "$Bin/../harbor/";
+	$yaml_file = $yaml_path.'nfs-client-provisioner-pv.yaml';
+	$tmpl_file = $yaml_file.'.tmpl';
+	if (!-e $tmpl_file) {
+		log_print("The template file [$tmpl_file] does not exist!\n");
+		exit;
+	}
+	$template = `cat $tmpl_file`;
+	$template =~ s/{{nfs_ip}}/$nfs_ip/g;
+	$template =~ s/{{nfs_dir}}/$nfs_dir/g;
+	#log_print("-----\n$template\n-----\n\n");
+	open(FH, '>', $yaml_file) or die $!;
+	print FH $template;
+	close(FH);
+	$cmd = "kubectl apply -f $yaml_path";
+	log_print("Deploy Harbor..\n");
+	$cmd_msg = `$cmd`;
+	log_print("-----\n$cmd_msg-----\n");
+
+	# Display Wait 3 min. message
+	log_print("It takes 1 to 3 minutes to deploy Harbor service. Please wait.. \n");
+
+	return;
+}
+
+sub create_dockerhub_proxy {
+# Add dockerhub Registry & create dockerhub Proxy Cache Project
+	$harbor_key = encode_base64("admin:$harbor_admin_password");
+	$harbor_key =~ s/\n|\r//;
+$cmd =<<END;
+curl -s -k --location --request POST 'https://$harbor_domain_name/api/v2.0/registries' --header 'Authorization: Basic $harbor_key' --header 'Content-Type: application/json' --data-raw '{
+  "name": "dockerhub",
+  "url": "https://hub.docker.com",
+  "insecure": false,
+  "type": "docker-hub",
+  "description": "Default Harbor Projcet Proxy Cache"
+}'
+END
+	$isRun=1;
+	$count=0;
+	while ($isRun && $count<10) {
+		$isRun=0;
+		$cmd_msg = `$cmd`;
+		if ($cmd_msg ne '' && index($cmd_msg, "'dockerhub' is already used")<=0) {
+			log_print("Add dockerhub Registry Error: $cmd_msg");
+			sleep(5);
+			$count ++;
+			$isRun=1;
+		}
+	}
+	if (index($cmd_msg, "'dockerhub' is already used")>0) {
+		log_print("The dockerhub Registry is already exists, skip adding.\n");
+	}
+	else {
+		log_print("Add dockerhub Registry OK.\n");
+	}
+	
+$cmd =<<END;
+curl -s -k --location --request POST 'https://$harbor_domain_name/api/v2.0/projects' --header 'Authorization: Basic $harbor_key' --header 'Content-Type: application/json' --data-raw '{
+  "project_name": "dockerhub",
+  "registry_id": 1,
+  "storage_limit": -1,
+  "metadata": {
+    "enable_content_trust": "false",
+	"auto_scan": "true",
+	"reuse_sys_cve_whitelist": "true",
+	"public": "true"
+  },
+  "public": true
+}'
+END
+	$isRun=1;
+	$count=0;
+	while ($isRun && $count<10) {
+		$isRun=0;
+		$cmd_msg = `$cmd`;
+		if ($cmd_msg ne '' && index($cmd_msg, "The project named dockerhub already exists")<=0) {
+			log_print("Create dockerhub Proxy Cache Project Error: $cmd_msg");
+			sleep(5);
+			$count ++;
+			$isRun=1;
+		}
+	}
+	if (index($cmd_msg, "The project named dockerhub already exists")>0) {
+		log_print("The dockerhub Projcet is already exists, skip adding.\n");
+	}
+	else {
+		log_print("Add dockerhub Projcet OK.\n");
+	}
+	
+	return;
+}
+
+sub log_print {
+	my ($p_msg) = @_;
+
+    print "$p_msg";
+	
+	open(FH, '>>', $logfile) or die $!;
+	print FH $p_msg;
+	close(FH);	
+
+    return;
+}
