ec76cde0886a4de77742506157e6a75918e072fa
jonathan <tryweb@ichiayi.com>
2021-01-25 20:28:10 +0800

    restart docker first and then wait for the Harbor service to be ready to create the dockerhub proxy project

--- harbor/install_harbor.pl
+++ harbor/install_harbor.pl
@@ -61,11 +61,12 @@ if ($isChk) {
 }
 log_print("Successfully deployed Harbor!\n");
 
+# add insecure-registries
+system("sudo $Bin/add-insecure-registries.pl");
+
 # create dockerhub proxy project
 create_dockerhub_proxy();
 
-# add insecure-registries
-system("sudo $Bin/add-insecure-registries.pl");
 
 exit;
 
