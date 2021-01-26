516ce52daa7d1b22819d124a2748855e96f34140
jonathan <tryweb@ichiayi.com>
2021-01-26 19:51:23 +0800

    if harbor_domain_name is defined, both Harbor IP:Port and harbor_domain_name are added in to insecure_registries

--- bin/add-insecure-registries.pl
+++ bin/add-insecure-registries.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl
 # add insecure-registries script
 #
-# Usage: sudo add-insecure-registries.pl <harbor_ip>
+# Usage: sudo add-insecure-registries.pl <harbor_ip> <harbor_domain_name>
 #
 use FindBin qw($Bin);
 
 $prgname = substr($0, rindex($0,"/")+1);
 if (!defined($ARGV[0])) {
-	print("Usage:	sudo $prgname <harbor_ip>\n");
+	print("Usage:	sudo $prgname <harbor_ip> <harbor_domain_name>\n");
 	exit;
 }
 $logfile = "$Bin/$prgname.log";
@@ -16,10 +16,11 @@ log_print("\n----------------------------------------\n");
 log_print(`TZ='Asia/Taipei' date`);
 
 $harbor_ip = $ARGV[0];
+$harbor_domain_name = (defined($ARGV[1])?$ARGV[1]:'';
 
 #sudo vi /etc/docker/daemon.json
 #{
-#    "insecure-registries":["10.20.0.71:32443"]
+#    "insecure-registries":["10.20.0.71:32443", "harbor.iiidevops.org"]
 #}
 $docker_daemon_json = "/etc/docker/daemon.json";
 if (-e $docker_daemon_json) {
@@ -36,9 +37,13 @@ if (-e $docker_daemon_json) {
 }
 
 # New file
+$insecure_registries = "\"$harbor_ip:32443\"";
+if ($harbor_domain_name ne '') {
+	$insecure_registries .= ", \"$harbor_domain_name\"";
+}
 $new_daemon_json =<<END;
 {
-    "insecure-registries":["$harbor_ip:32443"]
+    "insecure-registries":[$insecure_registries]
 }
 
 END
