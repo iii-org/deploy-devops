312cd1a811e7b325c5f6f3e2cfa3edfc25e3a9e5
jonathan <tryweb@ichiayi.com>
2021-02-19 22:59:00 +0800

    Add new function - sync GitHub iii-org-app to local Gitlab script

---
+++ bin/sync-iii-org-app.pl
@@ -0,0 +1,186 @@
+#!/usr/bin/perl
+# sync iii-org-app script
+#
+# Usage: sync-iii-org-app.pl [github_org] [gitubuser:github_token]
+#
+use FindBin qw($Bin);
+use JSON::MaybeXS qw(encode_json decode_json);
+my $p_config = "$Bin/../env.pl";
+if (!-e $p_config) {
+	print("The configuration file [$p_config] does not exist!\n");
+	exit;
+}
+require($p_config);
+
+$prgname = substr($0, rindex($0,"/")+1);
+$logfile = "$Bin/$prgname.log";
+
+log_print("\n----------------------------------------\n");
+log_print(`TZ='Asia/Taipei' date`);
+
+$github_org = (defined($ARGV[0]))?$ARGV[0]:'iii-org-app';
+$github_user_token = (defined($ARGV[1]))?$ARGV[1]:'';
+
+# Get GitHub org $github_org (iii-org-app) repo list
+# curl -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/orgs/iii-org-app/repos
+$arg_str = ($github_user_token ne '')?"-u $github_user_token ":'';
+$cmd = "curl -s $arg_str -H \"Accept: application/vnd.github.inertia-preview+json\" https://api.github.com/orgs/$github_org/repos";
+log_print("Get GitHub org $github_org repo list..\n");
+$cmd_msg = `$cmd`;
+if (index($cmd_msg, 'node_id')<0) {
+	log_print("Get GitHub org [$github_org] repos Error!\n---\n$cmd_msg\n---\n");
+	exit;
+}
+$hash_github_repo = decode_json($cmd_msg);
+$repo_num=0;
+foreach $repo_hash (@ {$hash_github_repo}) {
+	$repo_num++;
+	log_print("[$repo_num]:".$repo_hash->{'name'}."\n");
+}
+log_print("GitHub org [$github_org] : $repo_num repo(s)\n\n");
+if ($repo_num==0){
+	exit;
+}
+
+# Check if the GitLab group $github_org (iii-org-app) exists
+# curl --header "PRIVATE-TOKEN: QMi2xAxxxxxxxxxx-oaQ" https://gitlab-demo.iiidevops.org/api/v4/groups/
+$cmd = "curl -s --header \"PRIVATE-TOKEN: $gitlab_private_token\" http://$gitlab_ip:32080/api/v4/groups/";
+log_print("Get GitLab group list..\n");
+$cmd_msg = `$cmd`;
+$hash_msg = decode_json($cmd_msg);
+$group_list = '';
+foreach $group_hash (@ {$hash_msg}) {
+	$group_list .= '['.$group_hash->{'name'}.']';
+}
+if ($group_list eq '') {
+	log_print("---\n$cmd\n---\n$cmd_msg\n---\n");
+}
+else {
+	log_print("group_list : $group_list\n");
+}
+if (index($group_list, "[$github_org]")<0) {
+	# Create $github_org group
+	# curl -H "Content-Type: application/json" -H "PRIVATE-TOKEN: QMi2xAxxxxxxxxxx-oaQ" -X POST -d '{"name": "iii-org-app","path": "iii-org-app"}' https://gitlab-demo.iiidevops.org/api/v4/groups/
+	$cmd = "curl -s -H \"Content-Type: application/json\" -H \"PRIVATE-TOKEN: $gitlab_private_token\" -X POST -d '{\"name\": \"$github_org\",\"path\": \"$github_org\"}' http://$gitlab_ip:32080/api/v4/groups/";	
+	$cmd_msg = `$cmd`;
+	$ret = '';
+	if (index($cmd_msg, $github_org)>0){
+		$hash_msg = decode_json($cmd_msg);
+		$ret = $hash_msg->{'name'};
+		print("[$ret]\n");
+	}
+	if ($ret eq '' || $ret ne $github_org){
+		log_print("Add GitLab group [$github_org] Error!\n---\n$cmd_msg\n---\n");
+		exit;
+	}
+	else {
+		log_print("Add GitLab group [$github_org] OK!\n\n");
+	}
+}
+else {
+	log_print("GitLab group [$github_org] exists!\n\n");
+}
+
+
+# Get GitLab Group $github_org (iii-org-app) project list
+# curl --header "PRIVATE-TOKEN: QMi2xAxxxxxxxxxx-oaQ" https://gitlab-demo.iiidevops.org/api/v4/groups/iii-org-app/projects
+$cmd = "curl -s --header \"PRIVATE-TOKEN: $gitlab_private_token\" http://$gitlab_ip:32080/api/v4/groups/$github_org/projects";
+log_print("Get GitLab group $github_org project list..\n");
+$cmd_msg = `$cmd`;
+if (index($cmd_msg, '"message"')>0) {
+	log_print("Get GitLab group [$github_org] projects Error!\n---\n$cmd_msg\n---\n");
+	exit;
+}
+
+$hash_gitlab_project = decode_json($cmd_msg);
+$project_num=0;
+$prj_name_list = '';
+%hash_prj_id={};
+%hash_prj_path={};
+%hash_prj_created_at={};
+foreach $project_hash (@ {$hash_gitlab_project}) {
+	$project_num++;
+	$prj_name = $project_hash->{'name'};
+	$prj_name_list .= '['.$prj_name.']';
+	$hash_prj_id{$prj_name}=$project_hash->{'id'};
+	$hash_prj_path{$prj_name}=$project_hash->{'path'};
+	$hash_prj_created_at{$prj_name}=$project_hash->{'created_at'};
+	log_print("[$project_num]:".$project_hash->{'name'}."\n");
+}
+log_print("Gitlab group [$github_org] : $project_num project(s)\n\n");
+
+
+# Sync GitHub org -> GitLab group
+log_print("Sync GitHub org [$github_org] -> GitLab group [$github_org]..\n");
+$idx=0;
+foreach $repo_hash (@ {$hash_github_repo}) {
+	$idx++;
+	$repo_id = $repo_hash->{'id'};
+	$repo_name = $repo_hash->{'name'};
+	$repo_max_time = ($repo_hash->{'created_at'} gt $repo_hash->{'updated_at'})?$repo_hash->{'created_at'}:$repo_hash->{'updated_at'};
+	$repo_max_time = ($repo_max_time gt $repo_hash->{'pushed_at'})?$repo_max_time:$repo_hash->{'pushed_at'};
+	log_print("[$idx].	name:".$repo_name." ($repo_max_time)\n");
+	if (index($prj_name_list, "[$repo_name]")>=0) {
+		log_print("	GitLab-> id:".$hash_prj_id{$repo_name}." path:".$hash_prj_path{$repo_name}." created_at:".$hash_prj_created_at{$repo_name}."\n");
+		if ($repo_max_time gt $hash_prj_created_at{$repo_name}) {
+			update_github($hash_prj_id{$repo_name}, $repo_id, $repo_name, $github_org);
+			log_print("	update [$repo_name] OK!\n");
+		}
+	}
+	else {
+		import_github($repo_id, $repo_name, $github_org);
+		log_print("	import [$repo_name] OK!\n");
+	}
+}
+
+exit;
+
+sub update_github {
+	my ($p_gitlab_id, $p_repo_id, $p_new_name, $p_target_namespace) = @_;
+	my ($cmd, $cmd_msg);
+
+	#curl --request DELETE --header "PRIVATE-TOKEN: QMi2xAxxxxxxxxxx-oaQ" https://gitlab-demo.iiidevops.org/api/v4/projects/2
+	$cmd = "curl -s --request DELETE --header \"PRIVATE-TOKEN: $gitlab_private_token\" http://$gitlab_ip:32080/api/v4/projects/$p_gitlab_id";
+	$cmd_msg = `$cmd`;
+	if (index($cmd_msg, 'Accepted')<0) {
+		log_print("update_github:delete_gitlab [$p_new_name] Error!\n---\n$cmd\n---\n$cmd_msg\n---\n");
+		exit;
+	}
+	sleep(5);
+	import_github($p_repo_id, $p_new_name, $p_target_namespace);
+
+	return;
+}
+
+sub import_github {
+	my ($p_repo_id, $p_new_name, $p_target_namespace) = @_;
+	my ($cmd, $cmd_msg, $arg_user, $arg_token);
+
+	# curl --request POST --header "PRIVATE-TOKEN: QMi2xAxxxxxxxxxx-oaQ" --data "personal_access_token=de8b68c3ee3eccdf7d4d69c6260bff66482283a9&repo_id=336984846&new_name=django-postgresql-todo&target_namespace=iii-org-app" https://gitlab-demo.iiidevops.org/api/v4/import/github
+	if ($github_user_token eq '') {
+		($arg_user, $arg_token)=split(':', $github_user_token);
+	}
+	else {
+		$arg_token = 'de8b68c3ee3eccdf7d4d69c6260bff66482283a9';
+	}
+	$cmd = "curl -s --request POST --header \"PRIVATE-TOKEN: $gitlab_private_token\" --data \"personal_access_token=$arg_token&repo_id=$p_repo_id&new_name=$p_new_name&target_namespace=$p_target_namespace\" http://$gitlab_ip:32080/api/v4/import/github";
+	$cmd_msg = `$cmd`;
+	if (index($cmd_msg, $p_new_name)<0) {
+		log_print("import_github [$p_new_name] Error!\n---\n$cmd\n---\n$cmd_msg\n---\n");
+		exit;
+	}
+	
+	return;
+}
+
+sub log_print {
+	my ($p_msg) = @_;
+
+    print "$p_msg";
+	
+	open(FH, '>>', $logfile) or die $!;
+	print FH $p_msg;
+	close(FH);	
+
+    return;
+}
