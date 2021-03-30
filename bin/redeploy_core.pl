ed643fec80baec4488c2f2d6d1bf22e4e5afff8c
jonathan <tryweb@ichiayi.com>
2021-03-30 19:47:38 +0800

    Add redeploy_core script

---
+++ bin/redeploy_core.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/perl
+# redeploy core script
+#
+use FindBin qw($Bin);
+use MIME::Base64;
+my $p_config = "$Bin/../env.pl";
+if (!-e $p_config) {
+	print("The configuration file [$p_config] does not exist!\n");
+	exit;
+}
+require($p_config);
+
+$prgname = substr($0, rindex($0,"/")+1);
+$logfile = "$Bin/$prgname.log";
+$kubeconf_str = defined($ARGV[0])?'--kubeconfig '.$ARGV[0]:'';
+
+require("$Bin/../lib/common_lib.pl");
+log_print("\n----------------------------------------\n");
+log_print(`TZ='Asia/Taipei' date`);
+
+
+# iiidevops_ver
+$iiidevops_ver = ($iiidevops_ver eq '')?'1':$iiidevops_ver;
+
+# Deploy DevOps API (Python Flask) on kubernetes cluster
+$yaml_path = "$Bin/../devops-api/";
+$yaml_file = $yaml_path.'devopsapi-deployment.yaml';
+$tmpl_file = $yaml_file.'.tmpl';
+if (!-e $tmpl_file) {
+	log_print("The template file [$tmpl_file] does not exist!\n");
+	exit;
+}
+$template = `cat $tmpl_file`;
+$template =~ s/{{iiidevops_ver}}/$iiidevops_ver/g;
+$template =~ s/{{db_passwd}}/$db_passwd/g;
+$template =~ s/{{db_ip}}/$db_ip/g;
+$template =~ s/{{jwt_secret_key}}/$jwt_secret_key/g;
+$template =~ s/{{redmine_domain_name}}/$redmine_domain_name/g;
+$template =~ s/{{redmine_admin_passwd}}/$redmine_admin_passwd/g;
+$template =~ s/{{redmine_api_key}}/$redmine_api_key/g;
+$template =~ s/{{gitlab_url}}/$gitlab_domain_name/g;
+$template =~ s/{{gitlab_root_passwd}}/$gitlab_root_passwd/g;
+$template =~ s/{{gitlab_private_token}}/$gitlab_private_token/g;
+$template =~ s/{{rancher_ip}}/$rancher_ip/g;
+$template =~ s/{{rancher_admin_password}}/$rancher_admin_password/g;
+$template =~ s/{{harbor_ip}}/$harbor_ip/g;
+$template =~ s/{{harbor_domain_name}}/$harbor_domain_name/g;
+$template =~ s/{{harbor_admin_password}}/$harbor_admin_password/g;
+$template =~ s/{{k8sctl_domain_name}}/$k8sctl_domain_name/g;
+$template =~ s/{{ingress_domain_name}}/$ingress_domain_name/g;
+$template =~ s/{{checkmarx_origin}}/$checkmarx_origin/g;
+$template =~ s/{{checkmarx_username}}/$checkmarx_username/g;
+$template =~ s/{{checkmarx_password}}/$checkmarx_password/g;
+$template =~ s/{{checkmarx_secret}}/$checkmarx_secret/g;
+$template =~ s/{{webinspect_base_url}}/$webinspect_base_url/g;
+$template =~ s/{{sonarqube_domain_name}}/$sonarqube_domain_name/g;
+$template =~ s/{{sonarqube_admin_token}}/$sonarqube_admin_token/g;
+$template =~ s/{{admin_init_login}}/$admin_init_login/g;
+$template =~ s/{{admin_init_email}}/$admin_init_email/g;
+$template =~ s/{{admin_init_password}}/$admin_init_password/g;
+$template =~ s/{{nfs_ip}}/$nfs_ip/g;
+$template =~ s/{{nfs_dir}}/$nfs_dir/g;
+#log_print("-----\n$template\n-----\n\n");
+open(FH, '>', $yaml_file) or die $!;
+print FH $template;
+close(FH);
+$cmd = "kubectl $kubeconf_str apply -f $yaml_path";
+log_print("Deploy devops-api..[$cmd]\n");
+$cmd_msg = `$cmd`;
+log_print("-----\n$cmd_msg\n-----\n\n");
+
+
+# Deploy DevOps UI (VueJS) on kubernetes cluster
+$iiidevops_domain_name = get_domain_name('iiidevops');
+
+$yaml_path = "$Bin/../devops-ui/";
+$yaml_file = $yaml_path.'devopsui-deployment.yaml';
+$tmpl_file = $yaml_file.'.tmpl';
+if (!-e $tmpl_file) {
+	log_print("The template file [$tmpl_file] does not exist!\n");
+	exit;
+}
+$template = `cat $tmpl_file`;
+$template =~ s/{{iiidevops_ver}}/$iiidevops_ver/g;
+#log_print("-----\n$template\n-----\n\n");
+open(FH, '>', $yaml_file) or die $!;
+print FH $template;
+close(FH);
+
+$yaml_path = "$Bin/../devops-ui/";
+$yaml_file = $yaml_path.'devopsui-ingress.yaml';
+if ($deploy_mode ne '' && uc($deploy_mode) ne 'IP') {
+	$tmpl_file = $yaml_file.'.tmpl';
+	if (!-e $tmpl_file) {
+		log_print("The template file [$tmpl_file] does not exist!\n");
+		exit;
+	}
+	$template = `cat $tmpl_file`;
+	$template =~ s/{{iiidevops_domain_name}}/$iiidevops_domain_name/g;
+	#log_print("-----\n$template\n-----\n\n");
+	open(FH, '>', $yaml_file) or die $!;
+	print FH $template;
+	close(FH);
+}
+else {
+	$cmd = "rm -f $yaml_file";
+	$cmd_msg = `$cmd 2>&1`;
+	if ($cmd_msg ne '') {
+		log_print("$cmd Error!\n$cmd_msg-----\n");
+	}
+}
+
+$cmd = "kubectl $kubeconf_str apply -f $yaml_path";
+log_print("Deploy devops-ui..[$cmd]\n");
+$cmd_msg = `$cmd`;
+log_print("-----\n$cmd_msg\n-----\n\n");
+
