5b604759f60d5b166e154a4382dec8fb801866d1
jonathan <tryweb@ichiayi.com>
2021-04-13 18:21:17 +0800

    Change Install K8s method

--- bin/iiidevops_install_base.pl
+++ bin/iiidevops_install_base.pl
@@ -24,48 +24,81 @@ if ($cmd_msg ne '') {
 }
 log_print("Create data directory OK!\n");
 
-
 # NFS
-$cmd = "sudo $home/deploy-devops/bin/ubuntu20lts_install_nfsd.pl";
-log_print("\nInstall & Setting NFS service..");
-#$cmd_msg = `$cmd`;
-system($cmd);
-#log_print("-----\n$cmd_msg\n-----\n");
-# Check NFS service is working
-$cmd = "showmount -e $nfs_ip";
-$chk_key = $nfs_dir;
-$cmd_msg = `$cmd 2>&1`;
-log_print("-----\n$cmd_msg-----\n");
-if (index($cmd_msg, $chk_key)<0) {
-	log_print("NFS configuration failed!\n");
-	exit;
+if ($first_ip eq $nfs_ip) {
+	$cmd = "sudo $home/deploy-devops/bin/ubuntu20lts_install_nfsd.pl";
+	log_print("\nInstall & Setting NFS service..");
+	system($cmd);
+	# Check NFS service is working
+	$cmd = "showmount -e $nfs_ip";
+	$chk_key = $nfs_dir;
+	$cmd_msg = `$cmd 2>&1`;
+	log_print("-----\n$cmd_msg-----\n");
+	if (index($cmd_msg, $chk_key)<0) {
+		log_print("NFS configuration failed!\n");
+		exit;
+	}
+	log_print("NFS configuration OK!\n");
 }
-log_print("NFS configuration OK!\n");
+else {
+	log_print("Skip NFS configuration!\n");
+}
 
-# add insecure-registries
+# Add insecure-registries
 if ($harbor_ip ne '') {
 	system("sudo $Bin/add-insecure-registries.pl $harbor_ip $harbor_domain_name");
 }
 
-# # Rancher
-# $cmd = "sudo $home/deploy-devops/rancher/install_rancher.pl";
-# log_print("\nInstall Rancher..");
-# #$cmd_msg = `$cmd`;
-# system($cmd);
-# #log_print("-----\n$cmd_msg\n-----\n");
-# $cmd = "nc -z -v $rancher_ip 3443";
-# $chk_key = 'succeeded!';
-# $cmd_msg = `$cmd 2>&1`;
-# # Connection to 10.20.0.71 3443 port [tcp/*] succeeded!
-# if (index($cmd_msg, $chk_key)<0) {
-	# log_print("Failed to deploy Rancher!\n");
-	# log_print("-----\n$cmd_msg-----\n");
-	# exit;	
-# }
-# log_print("Successfully deployed Rancher!\n");
+# Gen K8s ssh key
+if (-e "$nfs_dir/deploy-config/id_rsa") {
+	$cmd = "cp -a $nfs_dir/deploy-config/ ~/.ssh/";
+}
+else {
+	$cmd = "ssh-keygen -t rsa -C '$admin_init_email'; cp -a ~/.ssh/id_rsa $nfs_dir/deploy-config/";
+}
+system($cmd);
 
-#log_print("\nThe deployment of NFS / Rancher services has been completed, These services URL are: \n");
-#log_print("Rancher - https://$rancher_ip:3443/\n");
+# Copy ssh key to first_ip
+$cmd = "ssh-copy-id rkeuser@$first_ip";
+system($cmd);
+# Verify rkeuser permission for running docker 
+$cmd = "ssh rkeuser@$first_ip; docker ps; exit";
+$chk_key = 'CREATED';
+$cmd_msg = `$cmd 2>&1`;
+#CONTAINER ID        IMAGE               COMMAND             CREATED             STATUS              PORTS               NAMES
+if (index($cmd_msg, $chk_key)<0) {
+	log_print("Verify rkeuser permission for running docker failed!\n");
+	exit;
+}
+log_print("Verify rkeuser permission for running docker OK!\n");
+
+# Install K8s
+system("$Bin/../kubernetes/update-k8s-cluster.pl Initial $first_ip");
+
+$cmd = "cp ~/kube_config_cluster.yml $nfs_dir/kube-config/config; ln -s $nfs_dir/kube-config/config ~/.kube/config";
+# Verify kubeconf
+$cmd = 'kubectl get node 2>&1';
+#NAME         STATUS   ROLES                      AGE   VERSION
+#10.20.0.37   Ready    controlplane,etcd,worker   49m   v1.18.17
+$chk_key = 'Ready';
+$isChk=1;
+$count=0;
+$wait_sec=600;
+while($isChk && $count<$wait_sec) {
+	log_print('.');
+	$cmd_msg = `$cmd 2>&1`;
+	$isChk = (index($cmd_msg, $chk_key)<0)?3:0;
+	$count = $count + $isChk;
+	sleep($isChk);
+}
+log_print("\n");
+if ($isChk) {
+	log_print("Failed to deploy K8s!\n");
+	log_print("-----\n$cmd_msg-----\n");
+	exit;
+}
+log_print("Successfully deployed K8s!\n");
+
 log_print("\nplease Read https://github.com/iii-org/deploy-devops/blob/master/README.md Step 4. to continue.\n\n");
 
 exit;
