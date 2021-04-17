ffab42b1a80e16a38c38e358fe549e535304c552
jonathan <tryweb@ichiayi.com>
2021-04-17 17:27:16 +0800

    Using get_service_status() to check service status after services installed

--- bin/iiidevops_install_cpnt.pl
+++ bin/iiidevops_install_cpnt.pl
@@ -21,92 +21,55 @@ $cmd = "$home/deploy-devops/rancher/install_rancher.pl";
 log_print("\nDeploy Rancher..");
 system($cmd);
 # Check Rancher service is working
-$rancher_domain_name = get_domain_name('rancher');
-$cmd = "nc -z -v $rancher_ip 31443 2>&1";
-$chk_key = 'succeeded!';
-$cmd_msg = `$cmd 2>&1`;
-if (index($cmd_msg, $chk_key)<0) {
-	log_print("Failed to deploy Rancher!\n");
-	log_print("-----\n$cmd_msg-----\n");
-	exit;	
+if (get_service_status('rancher')!=1) {
+	log_print("Rancher is not working!\n");
+	exit;
 }
-log_print("Successfully deployed Rancher!\n");
+log_print("Rancher is working well!\n");
 
 # Redmine
 $cmd = "$home/deploy-devops/redmine/install_redmine.pl";
 log_print("Deploy Redmine..");
-#$cmd_msg = `$cmd`;
 system($cmd);
-#log_print("-----\n$cmd_msg\n-----\n");
 # Check Redmine service is working
-$redmine_domain_name = get_domain_name('redmine');
-$cmd = "curl -q -I http://$redmine_domain_name";
-$chk_key = '200 OK';
-$cmd_msg = `$cmd 2>&1`;
-# HTTP/1.1 200 OK
-if (index($cmd_msg, $chk_key)<0) {
-	log_print("Failed to deploy Redmine!\n");
-	log_print("-----\n$cmd_msg-----\n");
-	exit;	
+if (get_service_status('redmine')!=1) {
+	log_print("Redmine is not working!\n");
+	exit;
 }
-log_print("Redmine..OK!\n\n");
+log_print("Redmine is working well!\n");
 
 # Harbor
 $cmd = "$home/deploy-devops/harbor/install_harbor.pl";
 log_print("\nDeploy Harbor..");
-#$cmd_msg = `$cmd`;
 system($cmd);
-#log_print("-----\n$cmd_msg\n-----\n");
 # Check Harbor service is working
-$harbor_domain_name = get_domain_name('harbor');
-$cmd = "curl -k --location --request POST 'https://$harbor_domain_name/api/v2.0/registries' 2>&1";
-$chk_key = 'UNAUTHORIZED';
-$cmd_msg = `$cmd 2>&1`;
-#{"errors":[{"code":"UNAUTHORIZED","message":"UnAuthorized"}]}
-if (index($cmd_msg, $chk_key)<0) {
-	log_print("Failed to deploy Harbor!\n");
-	log_print("-----\n$cmd_msg-----\n");
-	exit;	
+if (get_service_status('harbor')!=1) {
+	log_print("Harbor is not working!\n");
+	exit;
 }
-log_print("Successfully deployed Harbor!\n");
+log_print("Harbor is working well!\n");
 
 # Sonarqube
 $cmd = "$home/deploy-devops/sonarqube/install_sonarqube.pl";
 log_print("Deploy Sonarqube..");
-#$cmd_msg = `$cmd`;
 system($cmd);
-#log_print("-----\n$cmd_msg\n-----\n");
 # Check Sonarqube service is working
-$sonarqube_domain_name = get_domain_name('sonarqube');
-$cmd = "curl -q -I http://$sonarqube_domain_name";
-$chk_key = 'Content-Type: text/html;charset=utf-8';
-$cmd_msg = `$cmd 2>&1`;
-# Content-Type: text/html;charset=utf-8
-if (index($cmd_msg, $chk_key)<0) {
-	log_print("Failed to deploy Sonarqube!\n");
-	log_print("-----\n$cmd_msg-----\n");
-	exit;	
+if (get_service_status('sonarqube')!=1) {
+	log_print("Sonarqube is not working!\n");
+	exit;
 }
-log_print("Sonarqube ..OK!\n\n");
+log_print("Sonarqube is working well!\n");
 
 # GitLab
 $cmd = "$home/deploy-devops/gitlab/install_gitlab.pl";
 log_print("\nDeploy Gitlab..");
-#$cmd_msg = `$cmd`;
 system($cmd);
-#log_print("-----\n$cmd_msg\n-----\n");
 # Check GitLab service is working
-$gitlab_domain_name = get_domain_name('gitlab');
-$cmd = "curl -q -I http://$gitlab_domain_name/users/sign_in";
-$chk_key = '200 OK';
-$cmd_msg = `$cmd 2>&1`;
-# HTTP/1.1 200 OK
-if (index($cmd_msg, $chk_key)<0) {
-	log_print("Failed to deploy GitLab!\n");
-	log_print("-----\n$cmd_msg-----\n");
-	exit;	
+if (get_service_status('gitlab')!=1) {
+	log_print("GitLab is not working!\n");
+	exit;
 }
-log_print("Successfully deployed GitLab!\n");
+log_print("GitLab is working well!\n");
 
 log_print("The deployment of these services has been completed. The service URLs are: \n");
 log_print("Rancher - https://$rancher_domain_name/\n");
