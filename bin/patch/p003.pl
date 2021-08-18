783fb8cafbb6ff7d63e86de7c16f911468a607c3
QskytimQ <timwu000@gmail.com>
2021-08-18 08:51:19 +0800

    update patch p003 & update redmine install trackers name & add upgrade-iiidevops

--- bin/patch/p003.pl
+++ bin/patch/p003.pl
@@ -1,5 +1,19 @@
 #!/usr/bin/perl
 # update to V1.7.1 ISO Patch:
+use FindBin qw($Bin);
+$|=1; # force flush output
+
+my $p_config = "$Bin/../../env.pl";
+if (!-e $p_config) {
+	print("The configuration file [$p_config] does not exist!\n");
+	exit;
+}
+require($p_config);
+
+if ($redmine_ip eq '') {
+	print("The redmine_ip in [$p_config] is ''!\n\n");
+	exit;
+}
 
 # Check running user
 $cmd_msg = `whoami`;
@@ -9,10 +23,15 @@ if ($cmd_msg ne 'rkeuser') {
 	exit;
 }
 
+# Update Redmine trackers data
+$sql = "update trackers set name = 'Fail Management' WHERE id=9";
+$sql_cmd = "psql -d 'postgresql://postgres:$redmine_db_passwd\@$redmine_ip:32749/redmine' -c \"$sql\"";
+
 $cmd =<<END;
 cd ~
 ./deploy-devops/bin/generate_env.pl iiidevops_ver 1.7.1 -y;
 ./deploy-devops/bin/iiidevops_install_core.pl;
+$sql_cmd;
 ./deploy-devops/bin/sync_chart_index.pl gitlab_update
 END
 
---
+++ bin/upgrade-iiidevops.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# Upgrade iiidevops using bin/patch script
+#
+use FindBin qw($Bin);
+$|=1; # force flush output
+
+$prgname = substr($0, rindex($0,"/")+1);
+$ins_repo = (!defined($ARGV[0]))?'master':$ARGV[0];
+$logfile = "$Bin/$prgname.log";
+log_print("\n----------------------------------------\n");
+log_print(`TZ='Asia/Taipei' date`);
+
+# Check running user
+$cmd_msg = `whoami`;
+$cmd_msg =~ s/\n|\r//g;
+if ($cmd_msg ne 'rkeuser') {
+	log_print("You must use the 'rkeuser' account to run the installation script!\n");
+	exit;
+}
+
+# chdir "~/deploy-devops/bin/patch";
+$tmpl_list = `ls ~/deploy-devops/bin/patch/*.pl`;
+$patch_num = 0;
+$cmd = '';
+foreach $tmpl_name (split(".pl\n", $tmpl_list)) {
+	$cmd .= "perl $tmpl_name.pl;"
+}
+system($cmd);
+
+exit;
+
+sub log_print {
+	my ($p_msg) = @_;
+
+    print "$p_msg";
+	
+	open(FH, '>>', $logfile) or die $!;
+	print FH $p_msg;
+	close(FH);	
+
+    return;
+}
