3e6f05e4a5f004bdead6886d8bf1049b7ec3659c
jonathan <tryweb@ichiayi.com>
2022-03-23 11:28:34 +0800

    Check first to determine whether patch is needed to improve efficiency

--- bin/patch/p005.pl
+++ bin/patch/p005.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
-# 17:27 2021/12/29
-# gitlab DNS mod 
+# 10:31 2022/3/23
+# gitlab DNS mod / Redmine SECRET_KEY_BASE / SonarQube Liveness / Redis Store
 # [V]Auto
 # [ ]Manual
 use FindBin qw($Bin);
@@ -24,9 +24,36 @@ if ($cmd_msg ne 'rkeuser') {
 $cmd =<<END;
 $Bin/../../gitlab/install_gitlab.pl dns_set;
 $Bin/../../gitlab/install_gitlab.pl modify_ingress;
-$Bin/../../redmine/install_redmine.pl force
-$Bin/../../sonarqube/install_sonarqube.pl force
-kubectl apply -f $Bin/../../devops-redis
 END
-
 system($cmd);
+
+# Check redmine SECRET_KEY_BASE setting
+$cmd = "kubectl describe deployment redmine | grep REDMINE_SECRET_KEY_BASE";
+$cmd_msg = `$cmd 2>&1`;
+if ($cmd_msg eq '') {
+	$cmd = "$Bin/../../redmine/install_redmine.pl force";
+	system($cmd);
+}
+else {
+	print("The redmine SECRET_KEY_BASE setting already exists! Skip patch!\n");
+}
+
+# Check sonarqube-server Liveness setting
+$cmd = "kubectl describe deployment sonarqube-server | grep Liveness";
+$cmd_msg = `$cmd 2>&1`;
+if ($cmd_msg eq '') {
+	$cmd = "$Bin/../../sonarqube/install_sonarqube.pl force";
+	system($cmd);
+}
+else {
+	print("The sonarqube-server liveness setting already exists! Skip patch!\n");
+}
+
+# Check devops-redis dir
+if (!-e "$nfs_dir/devops-redis/") {
+	$cmd = "kubectl apply -f $Bin/../../devops-redis";
+	system($cmd);
+}
+else {
+	print("The redis storage directory already exists! Skip patch!\n");
+}
