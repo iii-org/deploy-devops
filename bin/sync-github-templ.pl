1ea8b726acf744edfb3dc052a3d83d6220a834ef
twhuang <twhuang@iii.org.tw>
2022-07-29 07:27:04 +0000

    Fix sync github perl

--- bin/sync-github-templ.pl
+++ bin/sync-github-templ.pl
@@ -59,13 +59,13 @@ else {
 $run_sync_prj = system("perl $Bin/sync-prj-templ.pl") >> 8;
 if ($run_sync_prj) {
     log_print("sync github iiidevops-templates... SUCCESS\n");
-) else {
+} else {
     log_print("sync github iiidevops-templates... FAIL\n");
 }
 log_print("----------------------------------------\n");
 $run_sync_chart = system("perl $Bin/sync_chart_index.pl gitlab_update") >> 8;
 if ($run_sync_chart) {
     log_print("sync sync github chart templates... SUCCESS\n");
-) else {
+} else {
     log_print("sync sync github chart templates... FAIL\n");
 }
